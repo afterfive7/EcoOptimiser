88ca99ce7ec62a9e33ab0c960c35fb155efc7665
afterfive <119972495+afterfive7@users.noreply.github.com>
2025-08-02 04:18:17 +0200

    gigabrain optimisation using fibonacci

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from optimiser import main
 
 # prefix = 'SEQ'; hq = 'Bloody Trail'
 # prefix = 'BFS'; hq = 'Azure Frontier'
-# prefix = None; hq = 'Cathedral Harbour'
-prefix = "ESI"; hq = 'Central Islands'
+prefix = None; hq = 'Cathedral Harbour'
+# prefix = "ESI"; hq = 'Central Islands'
 
 
 
-# main.from_api(prefix, hq)
-main.from_import("input.json")
+main.from_api(prefix, hq)
+# main.from_import("input.json")
--- optimiser/optimiser.py
+++ optimiser/optimiser.py
@@ -4,10 +4,6 @@ import json
 
 resources = ['emeralds'] + ["ore", "wood", "fish", "crops"]
 
-with open('data/resource_upgrades.json') as f:
-    upgrades = json.load(f)
-with open('data/upgrades.json') as f:
-    upgrade_costs = json.load(f)
 
 def optimise_upgrades(territories):
     solver = pywraplp.Solver.CreateSolver("SCIP")
@@ -15,89 +11,121 @@ def optimise_upgrades(territories):
     if not solver:
         raise RuntimeError("Solver not created.")
 
-    # Decision variables: x[t][u][r][e] = 1 if territory i gets upgrade u with rate r and efficiency e
+    with open('data/resource_upgrades.json') as f:
+        upgrades = json.load(f)
+    with open('data/upgrades.json') as f:
+        upgrade_costs = json.load(f)
+
+    # Create resource categories
+    categories = {}
+    category_counter = {}
+    for u in upgrades:
+        categories[u] = []
+        category_counter[u] = []
+    for t in territories:
+        territories[t]['categories'] = {}
+        for u in upgrades:
+            territories[t]['categories'][u] = {}
+            res = upgrades[u]["bonus_resources"]
+            terr_res = [territories[t]['production'][r] for r in res]
+            c = dict(zip(res, terr_res))
+            if c not in categories[u]:
+                categories[u].append(c)
+                category_counter[u].append([])
+            index = categories[u].index(c)
+            territories[t]['categories'][u]['index'] = index
+            # grouping in groups with sizes following fibonacci!!!
+            # 1,1,2,3,5,8,13,21,34
+            groups = category_counter[u][index]
+            if len(groups) <= 2:
+                groups.append(1)
+            elif groups[-3] + groups[-2] < groups[-1] + 1:
+                groups.append(1)
+            else:
+                groups[-1] += 1
+            territories[t]['categories'][u]['group'] = len(groups) - 1
+
+    # print(category_counter)
+    # print(categories)
+    for u in upgrades:
+        n = sum(len(x) for x in category_counter[u])
+        print(f"number of groups in upgrade type {u}: {n}")
+    # for t in territories:
+    #     print(territories[t]['categories'])
+
+
+    # Decision variables: x[u, c, g, r, e] = 1 if territory in category c, group g gets upgrade u with rate r and efficiency e
     x = {}
     nk = {k: (len(v['costs']), len(v['costs'][0])) for k, v in upgrades.items()}
 
-    for t in territories:
-        for u in upgrades:
-            for r in range(nk[u][0]):  # rate
-                for e in range(nk[u][1]):  # efficiency
-                    x[t, u, r, e] = solver.IntVar(0, 1, f"x_{t}_{u}_{r}_{e}")
-
-    # Constraint: only one upgrade per territory
-    for t in territories:
-        for u in upgrades:
-            solver.Add(solver.Sum(x[t, u, r, e] for r in range(nk[u][0]) for e in range(nk[u][1])) == 1)
-
-    # Constraint: total emerald cost must be within budget
-    # each resource cost < prod
-    # add defense costs etc later
     res_prod = {res:[] for res in resources}
     res_cost = {res:[] for res in resources}
-    terr_cost = {}
-    for t, territory in territories.items():
-        terr_cost[t]= {res:[] for res in resources}
-        for res in resources:
-            p = territory['production'][res]
-            if p > 0:
-                res_prod[res].append(p)
-            for u, upgrade in upgrades.items():
-                for r in range(nk[u][0]):
-                    for e in range(nk[u][1]):
-                        if res in upgrade["bonus_resources"]: # Upgrade bonusses
-                            bonus = int(p*upgrade['bonus'][r][e])
-                            if bonus > 0:
-                                res_prod[res].append(bonus*x[t, u, r, e])
-                        if res in upgrade['costs'][r][e]: # Upgrade costs
-                            cost = upgrade['costs'][r][e][res]
-                            if cost > 0:
-                                res_cost[res].append(cost*x[t, u, r, e])
-                                terr_cost[t][res].append(cost*x[t, u, r, e])
 
-        # Calculate cost caused by storage requirements
-        pem = territory['production']['emeralds']
-        pmax = max(territory['production'][res] for res in resources[1::])
+    for u in upgrades:  # upgrade type u
+        for c, prod in enumerate(categories[u]):  # category index c
+            counter = category_counter[u][c]
+            for g, n in enumerate(counter):  # fibonacci group g with n territories
+                for r in range(nk[u][0]):  # rate upgrade
+                    for e in range(nk[u][1]):  # efficiency upgrade
+                        x[u, c, g, r, e] = solver.IntVar(0, 1, f"x_{u}_{c}_{g}_{r}_{e}")
+                # Select exactly one upgrade level
+                solver.Add(solver.Sum(x[u, c, g, r, e] for r in range(nk[u][0]) for e in range(nk[u][1])) == 1)
 
-        for r in range(nk['Emeralds'][0]):
-            for e in range(nk['Emeralds'][1]):
-                bonus = int(pem*upgrades['Emeralds']['bonus'][r][e])
-                # ignore additional storage requirement from emerald usage. This seems to complex
-                level = territory["upgrades"].get("emeraldStorage",0)
-                storage_cost = get_storage_cost(pem + bonus, em=True, hq=(territory['distance']==0), level=level)
-                if storage_cost > 0:
-                    res_cost['wood'].append(storage_cost*x[t, 'Emeralds', r, e])
-                    terr_cost[t]['wood'].append(storage_cost*x[t, 'Emeralds', r, e])
+                # Generate production bonuses and associated costs
+                for res in upgrades[u]["bonus_resources"]:  # iterate through resources boosted by the upgrade
+                    p = prod[res]
+                    # print(u, c, g, res, p)
+                    if p > 0:
+                        res_prod[res].append(n*p)  # base production (number of territories in group) x production
+                        for r in range(nk[u][0]):  # rate upgrade
+                            for e in range(nk[u][1]):  # efficiency upgrade
+                                bonus = p*upgrades[u]['bonus'][r][e]
+                                costs = upgrades[u]['costs'][r][e]
+                                res_prod[res].append(n*bonus*x[u, c, g, r, e])
+                                for ress, cost in costs.items():
+                                    res_cost[ress].append(n*cost*x[u, c, g, r, e])
+                # Account for storage requirements (tbh this is now fucked up a bit but storages don't matter anyways)
+                # Also we still only look at storage requirements from production. Costs would make this multiplicative
+                pmax = max(prod.values())
+                for r in range(nk[u][0]):  # rate upgrade
+                    for e in range(nk[u][1]):  # efficiency upgrade
+                        bonus = pmax*upgrades[u]['bonus'][r][e]
+                        storage_cost = 0
+                        for terr in territories.values():
+                            if terr['categories'][u]['index'] == c and terr['categories'][u]['group'] == g:
+                                if u == "Emeralds":
+                                    level = terr["upgrades"].get("emeraldStorage",0)
+                                else:
+                                    level = terr["upgrades"].get("resourceStorage",0)
+                                storage_cost += get_storage_cost(pmax + bonus, u, hq=(terr['distance']==0), level=level)
+                        if storage_cost > 0:
+                            if u == "Emeralds":
+                                res_cost['wood'].append(storage_cost*x[u, c, g, r, e])
+                            else:
+                                res_cost['wood'].append(storage_cost*x[u, c, g, r, e])
 
-        for r in range(nk['Resources'][0]):
-            for e in range(nk['Resources'][1]):
-                bonus = int(pmax*upgrades['Resources']['bonus'][r][e])
-                level = territory["upgrades"].get("resourceStorage",0)
-                storage_cost = get_storage_cost(pmax + bonus, hq=(territory['distance']==0), level=level)
-                if storage_cost > 0:
-                    res_cost['emeralds'].append(storage_cost*x[t, 'Resources', r, e])
-                    terr_cost[t]['emeralds'].append(storage_cost*x[t, 'Resources', r, e])
-
-        # Add costs for additional upgrades
+    # Add costs for additional upgrades
+    for territory in territories.values():
         for u,v in territory['upgrades'].items():
             res = upgrade_costs[u]["resource"]
             cost = upgrade_costs[u]['costs'][v]
             res_cost[res].append(cost)
-            terr_cost[t][res].append(cost)
+
 
     res_prod_sum = {}
     res_cost_sum = {}
-    extra_surpluss = {'emeralds':0, "ore":0, "crops":0, "fish":0, "wood":0}
+    extra_surplus = {'emeralds':0, "ore":0, "crops":0, "fish":0, "wood":0}
     for res in resources:
         res_prod_sum[res] = solver.Sum(res_prod[res])
         res_cost_sum[res] = solver.Sum(res_cost[res])
-        solver.Add(res_prod_sum[res] >= res_cost_sum[res] + extra_surpluss[res])
-        # solver.Add(res_prod_sum[res] <= solver.Sum([res_cost_sum[res], 100000]))
+        solver.Add(res_prod_sum[res] >= res_cost_sum[res] + extra_surplus[res])
+        # solver.Add(res_prod_sum[res] <= res_cost_sum[res] + extra_surplus[res] + 10000)
 
 
     # Objective: maximize total boosted resource production with weights
     tot_sum = []
-    weights = {'emeralds':0, "ore":6, "crops":15, "fish":12, "wood":4}
+    # weights = {'emeralds':0, "ore":6, "crops":15, "fish":12, "wood":4}
+    weights = {'emeralds':0, "ore":1,  "crops":1, "fish":1, "wood":1}
     for res in resources:
         tot_sum.append(res_prod_sum[res]*weights[res] - res_cost_sum[res]*weights[res])
     objective = solver.Sum(tot_sum)
@@ -113,12 +141,16 @@ def optimise_upgrades(territories):
         for res in resources:
             print(f"{res} prod: {res_prod_sum[res].solution_value()}, cost: {res_cost_sum[res].solution_value()}")
         for t in territories:
+            # print(territories[t]['categories'])
             for u, upgrade in upgrades.items():
-                sol = np.array([[x[t, u, r, e].solution_value() for e in range(nk[u][1])] for r in range(nk[u][0])])
+                c = territories[t]['categories'][u]['index']
+                g = territories[t]['categories'][u]['group']
+                sol = np.array([[x[u, c, g, r, e].solution_value() for e in range(nk[u][1])] for r in range(nk[u][0])])
                 for i, ut in enumerate(upgrade["upgrades"]):
                     territories[t]['upgrades'][ut] = int(np.argwhere(sol == 1)[0][i])
     else:
         print("❌ No optimal solution found.")
+        print(status)
 
     return territories
 
@@ -136,7 +168,10 @@ storage_levels = [
     (34, 16000),
     (80, 48000),
 ]
-def get_storage_cost(prod, em=False, hq=False, level=0):
+def get_storage_cost(prod, upgrade, hq=False, level=0):
+    em = False
+    if upgrade == "Emeralds":
+        em = True
     base = 300
     if em: base = 3000
     if hq: base = 1500
